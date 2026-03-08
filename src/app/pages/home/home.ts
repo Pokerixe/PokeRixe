340985bb61dc9f589a091d94415059f4bf5110d7
BaptisteDPS <baptiste.dupuuis2406@gmail.com>
2026-03-08 16:19:34 +0100

    :sparkles: add api response class

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import { Subscription } from 'rxjs';
-import { PokeApiService } from '../../shared/services/pokeApi-service';
 
 @Component({
   selector: 'app-home',
@@ -8,32 +7,6 @@ import { PokeApiService } from '../../shared/services/pokeApi-service';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnInit, OnDestroy {
-  private sub?: Subscription;
-  isLoading = false;
-  errorMessage: string | null = null;
 
-  constructor(private pokeService: PokeApiService) {}
-
-  ngOnInit() {
-    this.isLoading = true;
-    this.sub = this.pokeService.getPokedex().subscribe({
-      next: (pokedex) => {
-        console.log('PokeApiService.getPokedex result:', pokedex);
-        this.isLoading = false;
-      },
-      error: (err) => {
-        console.error('Error fetching pokedex:', err);
-        this.errorMessage = err?.message || 'Erreur lors du chargement du Pokédex';
-        this.isLoading = false;
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.sub) {
-      this.sub.unsubscribe();
-      this.sub = undefined;
-    }
-  }
+export class Home {
 }
