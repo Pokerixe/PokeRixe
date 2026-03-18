0290abb63c5c414469255293bb2eb6cea20a7cc0
BaptisteDPS <baptiste.dupuuis2406@gmail.com>
2026-03-18 10:19:36 +0100

    :bug: fix deploy, will use mock for prodution

--- src/app/core/models/auth.model.ts
+++ src/app/core/models/auth.model.ts
@@ -1,24 +1,34 @@
-
-
 import {User} from './user.model';
 
-/** * DTO (Data Transfer Object) type qu'on utilise pour se connecter, s'inscrire etc...
- * - LoginDTO : Contient les champs nécessaires pour se connecter (email, password)
- * - RegisterDTO : Contient les champs nécessaires pour s'inscrire (name, email, password)
- * - AuthResponse : Contient les données retournées par le serveur après une opération d'authentification (qdqdqd)
+/**
+ * DTO (Data Transfer Object) pour les opérations d'authentification.
+ * contient les champs de connexion
+ * - email : L'adresse e-mail de l'utilisateur.
+ * - password : Le mot de passe de l'utilisateur.
  */
-
 export interface LoginDTO {
   email: string;
   password: string;
 }
 
+/**
+ * DTO (Data Transfer Object) pour les opérations d'inscriptions.
+ * contient les champs d'inscription
+ * - name : Nom de l'utilisateur.
+ * - email : L'adresse e-mail de l'utilisateur.
+ * - password : Le mot de passe de l'utilisateur.
+ */
 export interface RegisterDTO {
   name: string;
   email: string;
   password: string;
 }
 
+/**
+ * DTO (Data Transfer Object) pour la réponse d'authentification.
+ * contient les données retournées par le serveur après une opération d'authentification.
+ * - user : Les informations de l'utilisateur authentifié (id, name, email, etc.).
+ */
 export interface AuthResponse {
   user : User;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
-  production: true,
-  useMockApi: false,
+  production: false,
+  useMockApi: true,
   apiUrl: 'https://ton-vrai-backend.com/api/'
 };
+
+//export const environment = {
+//   production: true,
+//   useMockApi: false,
+//   apiUrl: 'https://ton-vrai-backend.com/api/'
+// };
