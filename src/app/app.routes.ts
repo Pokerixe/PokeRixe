42c76d0acb7ad18ac2afd1ed312c0e3d468cf2f9
BaptouK <baptiste.dupuis2406@gmail.com>
2026-03-19 19:37:34 +0100

    :sparkles: Equipes page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   },
   {
     path: 'equipes',
-    canActivate: [authGuard],
+    //canActivate: [authGuard],
     loadComponent: () => import('./pages/equipes/equipes').then(m => m.Equipes)
   },
   {
--- src/app/pages/equipes/equipes.ts
+++ src/app/pages/equipes/equipes.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import {Card} from '../../shared/components/card/card';
+
+export enum EquipeMode {
+  CHOIX_ATTACK = 'choix attack',
+  CHOIX_POKEMON = 'choix pokemon',
+  AFFICHAGE_POKEMON = 'affichage pokemon'
+}
 
 @Component({
   selector: 'app-equipes',
   imports: [
-    Card
+    Card,
+    NgIf
   ],
   templateUrl: './equipes.html',
   styleUrl: './equipes.css',
 })
 export class Equipes {
+  public Mode = EquipeMode;
+  public selectedMode: EquipeMode = EquipeMode.CHOIX_POKEMON;
+
+  cycleMode() {
+    const modes = Object.values(EquipeMode) as EquipeMode[];
+    const idx = modes.indexOf(this.selectedMode);
+    this.selectedMode = modes[(idx + 1) % modes.length];
+  }
+
+  setMode(mode: EquipeMode) {
+    this.selectedMode = mode;
+  }
 
 }
