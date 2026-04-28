fcd6bd40b48744713cad422119a35113b8e773c0
BaptouK <baptiste.dupuis2406@gmail.com>
2026-04-28 15:32:34 +0200

    :sparkles: add .env files and config support

---
+++ scripts/set-env.js
@@ -0,0 +1,30 @@
+const { writeFileSync } = require('fs');
+const { resolve } = require('path');
+require('dotenv').config({ path: resolve(__dirname, '../.env') });
+
+const dev = `export const environment = {
+  production: false,
+  useMockApi: false,
+  apiUrl: '${process.env['DEV_API_URL']}'
+};
+`;
+
+const prod = `export const environment = {
+  production: true,
+  useMockApi: false,
+  apiUrl: '${process.env['PROD_API_URL']}'
+};
+`;
+
+const staging = `export const environment = {
+  production: true,
+  useMockApi: true,
+  apiUrl: '${process.env['PROD_API_URL']}'
+};
+`;
+
+writeFileSync(resolve(__dirname, '../src/environments/environment.ts'), dev);
+writeFileSync(resolve(__dirname, '../src/environments/environment.prod.ts'), prod);
+writeFileSync(resolve(__dirname, '../src/environments/environment.staging.ts'), staging);
+
+console.log('✓ Environment files generated from .env');
